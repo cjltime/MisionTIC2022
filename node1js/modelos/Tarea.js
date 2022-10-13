1aa75273bd40b2191aca00e09d2e44e70d073b2b
Carlos Julio <ingedeservicios@yahoo.com>
2022-10-13 11:13:01 -0500

    INICIO DE MI API NODE1 FUNCIONANDO

---
+++ node1js/modelos/Tarea.js
@@ -0,0 +1,11 @@
+
+
+const mongoose = require("mongoose");
+
+let TareaSchema=new mongoose.Schema({
+    idTarea:Number,
+    nombreTarea:String,
+    detalleTarea:String
+});
+
+module.exports = mongoose.model("tarea",TareaSchema,"Tareas");
---
+++ node1js/servidor.js
@@ -0,0 +1,38 @@
+
+
+const express = require("express");
+const mongoose=require("mongoose");
+const TareaSchema=require("./modelos/Tarea.js");
+
+const app = express();
+const router=express.Router();
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//conexion base de datos
+
+mongoose.connect("mongodb+srv://cjltime:j7v3DGw0PhvIJM5R@clustercjltime.narasaq.mongodb.net/Actididades1DB?retryWrites=true&w=majority");
+
+// operaciones CRUD
+
+router.get("/", (req,res)=>{
+    res.send("El Inicio de mi API");
+})
+
+router.post("/tarea",(req,res)=>{
+    let nuevaTarea=new TareaSchema({
+        idTarea:req.body.id,
+        nombreTarea:req.body.nombre,
+        detalleTarea:req.body.detalle
+    });
+    nuevaTarea.save(function(err, datos){
+        if (err){
+            console.log(err);
+        }
+        res.send("Tarea Almacenada con Exito")
+    })
+});
+
+app.use(router);
+app.listen(3000, () => {
+    console.log("servidor corriendo en el puerto 3000")
+});
